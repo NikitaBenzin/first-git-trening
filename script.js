c2f73bd4ba8d668982d3489e95ab3c08753900fe
Ivan <example@gmail.com>
2023-01-05 20:55:00 +0100

    js update 01.05

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+//"use strict";
+let numberOfFilms;
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,12 +19,51 @@ const personalMovieDB = {
     privat: false
 };
 
-const firstLastSeeing = prompt("Один из последних просмотренных фильмов?", "");
-const ratingFirstLastSeeing = prompt("На сколько оцените его?");
-const secondLastSeeing = prompt("Один из последних просмотренных фильмов?", "");
-const ratingSecondLastSeeing = prompt("На сколько оцените его?");
+function rememberMyFilms(){
+    for(let i = 0; i < 2; i++){
+        const lastSeeing = prompt("Один из последних просмотренных фильмов?", ""),
+              ratingLastSeeing = prompt("На сколько оцените его?");
+    
+        if(lastSeeing != null && ratingLastSeeing != null && lastSeeing != '' && ratingLastSeeing != '' && lastSeeing.length < 50){
+            personalMovieDB.movies[lastSeeing] = ratingLastSeeing;
+            
+        } else {
+            i--;
+        }
+    }
+}
 
-personalMovieDB.movies[firstLastSeeing] = ratingFirstLastSeeing;
-personalMovieDB.movies[secondLastSeeing] = ratingSecondLastSeeing;
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function writeYouGenres(){
+    for(let i = 0; i < 2; i++){
+        const genresFilms = prompt(`Ваш любимый жанр под номером ${i+1}`);
+        if(genresFilms != null && genresFilms != '' && genresFilms.length < 50){
+            personalMovieDB.genres[i] = genresFilms;
+        } else {
+            i--;
+        }
+    }
+}
+writeYouGenres();
+function detectedPersonalLevel(){
+    if(personalMovieDB.count < 10){
+        alert("Просмотрено мало фильмов");
+    
+    } else if (personalMovieDB.count < 30){
+        alert("Ты лох!");
+    
+    } else {
+        alert("Ты полный лох!");
+    }
+}
+
+detectedPersonalLevel();
+
+function showMyDB(){
+    if(personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
